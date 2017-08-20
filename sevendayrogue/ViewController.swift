66876e172e69a50b604f540278fe5d5ae8560e18
Bart van Kuik <bart@dutchvirtual.nl>
2017-08-20 16:49:51 +0200

    Added empty project

---
+++ sevendayrogue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sevendayrogue
+//
+//  Created by Bart van Kuik on 20/08/2017.
+//  Copyright © 2017 DutchVirtual. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
