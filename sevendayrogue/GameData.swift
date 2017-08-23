afa395310f605412cbf98585a554e01b3b8ab798
Bart van Kuik <bart@dutchvirtual.nl>
2017-08-23 13:25:54 +0200

    Added world and data files for names

---
+++ sevendayrogue/GameData.swift
@@ -0,0 +1,9 @@
+//
+//  GameData.swift
+//  sevendayrogue
+//
+//  Created by Bart van Kuik on 23/08/2017.
+//  Copyright © 2017 DutchVirtual. All rights reserved.
+//
+
+import Foundation
---
+++ sevendayrogue/World.swift
@@ -0,0 +1,9 @@
+//
+//  World.swift
+//  sevendayrogue
+//
+//  Created by Bart van Kuik on 23/08/2017.
+//  Copyright © 2017 DutchVirtual. All rights reserved.
+//
+
+import Foundation
