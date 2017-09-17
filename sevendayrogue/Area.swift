f896a8caca7b4bfe11804c55d615af3b4fc398ec
Bart van Kuik <bart@dutchvirtual.nl>
2017-09-17 11:26:16 +0200

    Temporary commit before moving to Xcode 9

--- sevendayrogue/Area.swift
+++ sevendayrogue/Area.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+
+struct Area {
+    let origin: Location
+    let radius: Int
+
+    func contains(location: Location) -> Bool {
+        let containsX: Bool
+        let containsY: Bool
+
+        if (self.origin.x - radius) <= location.x && (self.origin.x + radius) >= location.x {
+            containsX = true
+        } else {
+            containsX = false
+        }
+
+        if (self.origin.y - radius) <= location.y && (self.origin.y + radius) >= location.y {
+            containsY = true
+        } else {
+            containsY = false
+        }
+
+        let containsXY = (containsX && containsY)
+        return containsXY
+    }
+}
